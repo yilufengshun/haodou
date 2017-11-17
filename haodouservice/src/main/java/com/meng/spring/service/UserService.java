5d2143c928d0a7f894872544507d1439b54bff21
919477713 <919477713@qq.com>
2017-11-17 20:02:04 +0800

    service

--- haodouservice/src/main/java/com/meng/spring/service/UserService.java
+++ haodouservice/src/main/java/com/meng/spring/service/UserService.java
@@ -1,7 +1,10 @@
 package com.meng.spring.service;
 
 import com.meng.spring.pojo.HdUser;
+import com.meng.spring.pojo.HdUserExample;
 
 public interface UserService {
     void addUser(HdUser hdUser);
+    HdUser selectUser(String phone);
+
 }
